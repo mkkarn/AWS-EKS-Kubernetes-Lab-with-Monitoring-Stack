d8ade22b4c1bbf573421c8aa7f19f6f279283e24
Manish karn <mkkarn1904@gmail.com>
2026-04-15 11:20:16 +0530

    Corrected backend and terraform.tfvars

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "your-unique-terraform-state-bucket-ap-south-1" # CHANGE THIS
+    bucket         = "EKS-manishk-bucket-ap-south-1" 
     key            = "eks-lab/dev/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock" # CHANGE THIS
+    dynamodb_table = "terraform-locks" 
   }
 }
