4dda79f4b383b11ada8feaee9619b2dbbbb2bbd7
Manish karn <mkkarn1904@gmail.com>
2026-04-15 13:21:11 +0530

    corrected vpc main.tf, outputs.tf, variables.tf and versions.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,28 +1,42 @@
+################################################################################
+# VPC Module Variables
+################################################################################
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC."
+  description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
+
+  validation {
+    condition     = can(cidrhost(var.vpc_cidr, 0))
+    error_message = "Must be a valid IPv4 CIDR block."
+  }
 }
 
 variable "az_count" {
-  description = "Number of availability zones to create subnets in."
+  description = "Number of availability zones to use"
   type        = number
   default     = 2
 
   validation {
-    condition     = var.az_count >= 1
-    error_message = "az_count must be at least 1."
+    condition     = var.az_count >= 2 && var.az_count <= 3
+    error_message = "AZ count must be between 2 and 3."
   }
 }
 
-variable "cluster_name" {
-  description = "Name of the cluster; used to name resources and tags."
-  type        = string
+variable "single_nat_gateway" {
+  description = "Use a single NAT Gateway for all private subnets (cost optimization)"
+  type        = bool
+  default     = true
 }
 
 variable "tags" {
-  description = "Additional tags to apply to all resources (merged with module tags)."
+  description = "Tags to apply to all resources"
   type        = map(string)
   default     = {}
 }
-
